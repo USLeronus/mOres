7b7b85bb5b3f7a57e86c2496aec1d6b500d2995e
Leronus <leronus@live.nl>
2024-03-17 04:24:49 +0100

    Simple order change in ShieldMaterials

--- src/main/java/mod/leronus/mores/item/ModShieldMaterials.java
+++ src/main/java/mod/leronus/mores/item/ModShieldMaterials.java
@@ -7,8 +7,8 @@ import net.minecraft.world.item.Rarity;
 
 public class ModShieldMaterials {
     public static final ModShieldMaterial TIN = new ModShieldMaterial("tin", 1, 84, ModTags.Items.TIN_INGOTS, 5, Rarity.COMMON);
-    public static final ModShieldMaterial SILVER = new ModShieldMaterial("silver", 2, 134, ModTags.Items.SILVER_INGOTS, 15, Rarity.COMMON);
     public static final ModShieldMaterial COPPER = new ModShieldMaterial("copper", 2, 112, Items.COPPER_INGOT, 10, Rarity.COMMON);
+    public static final ModShieldMaterial SILVER = new ModShieldMaterial("silver", 2, 134, ModTags.Items.SILVER_INGOTS, 15, Rarity.COMMON);
     public static final ModShieldMaterial BRONZE = new ModShieldMaterial("bronze", 3, 299, ModTags.Items.BRONZE_INGOTS, 13, Rarity.COMMON);
     public static final ModShieldMaterial COBALT = new ModShieldMaterial("cobalt", 4, 388, ModTags.Items.COBALT_INGOTS, 22, Rarity.COMMON);
     public static final ModShieldMaterial STERLING = new ModShieldMaterial("sterling", 4, 333, ModTags.Items.STERLING_INGOTS, 17, Rarity.COMMON);
@@ -19,8 +19,8 @@ public class ModShieldMaterials {
     public static final ModShieldMaterial TOPAZ = new ModShieldMaterial("topaz", 6, 632, ModTags.Items.TOPAZ_GEMS, 14, Rarity.COMMON);
     public static final ModShieldMaterial TOURMALINE = new ModShieldMaterial("tourmaline", 6, 621, ModTags.Items.TOURMALINE_GEMS, 16, Rarity.COMMON);
     public static final ModShieldMaterial TANZANITE = new ModShieldMaterial("tanzanite", 7, 784, ModTags.Items.TANZANITE_GEMS, 11, Rarity.COMMON);
-    public static final ModShieldMaterial SAPPHIRE = new ModShieldMaterial("sapphire", 7, 711, ModTags.Items.SAPPHIRE_GEMS, 18, Rarity.COMMON);
     public static final ModShieldMaterial RUBY = new ModShieldMaterial("ruby", 7, 809, ModTags.Items.RUBY_GEMS, 15, Rarity.COMMON);
+    public static final ModShieldMaterial SAPPHIRE = new ModShieldMaterial("sapphire", 7, 711, ModTags.Items.SAPPHIRE_GEMS, 18, Rarity.COMMON);
     public static final ModShieldMaterial OBSIDIAN = new ModShieldMaterial("obsidian", 7, 821, ModTags.Items.OBSIDIAN_INGOTS, 21, Rarity.COMMON);
     public static final ModShieldMaterial TURQUOISE = new ModShieldMaterial("turquoise", 7, 1000, ModTags.Items.TURQUOISE_GEMS, 25, Rarity.COMMON);
     public static final ModShieldMaterial MOISSANITE = new ModShieldMaterial("moissanite", 7, 913, ModTags.Items.MOISSANITE_GEMS, 10, Rarity.COMMON);
